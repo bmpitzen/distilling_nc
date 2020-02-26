847b934f86dab7b6bea840e1ac1e57e1a51fff09
Brandon Pitzen <bmpitzen@icloud.com>
2020-02-26 15:27:34 -0500

    Added menu to all pages & added about & contact.

--- app/controllers/distilleries_controller.rb
+++ app/controllers/distilleries_controller.rb
@@ -28,7 +28,7 @@ class DistilleriesController < ApplicationController
 
     respond_to do |format|
       if @distillery.save
-        format.html { redirect_to @distillery, notice: 'Distillery was successfully created.' }
+        format.html { redirect_to @distillery, class: "alert alert-info", role: "alert", notice: 'Distillery was successfully created.' }
         format.json { render :show, status: :created, location: @distillery }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class DistilleriesController < ApplicationController
   def update
     respond_to do |format|
       if @distillery.update(distillery_params)
-        format.html { redirect_to @distillery, notice: 'Distillery was successfully updated.' }
+        format.html { redirect_to @distillery, class: "alert alert-info", role: "alert", notice: 'Distillery was successfully updated.' }
         format.json { render :show, status: :ok, location: @distillery }
       else
         format.html { render :edit }
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :regions
   resources :distilleries
   root 'home#index'
+  get 'about', to: 'home#about'
+  get 'contact', to: 'home#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
