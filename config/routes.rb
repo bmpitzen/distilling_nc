e26b8f70eb888fb7629413f624d8c530ad32d2b7
Brandon Pitzen <bmpitzen@icloud.com>
2020-04-15 23:36:12 -0400

    Added concern to routes for nicer URLs in kaminari

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
+
+# This is to make nicer URLs in kaminari
+  concern :paginatable do
+    get "(page/:page)", action: :index, on: :collection, as: ""
+  end
+
   resources :spirits
   resources :spirit_types
   resources :regions
-  resources :distilleries
+  resources :distilleries, concerns: :paginatable
   root 'home#index'
   get 'about', to: 'home#about'
   get 'contact', to: 'home#contact'
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
